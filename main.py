0ba7a510a6d196487f3bbaf6376f510210548537
Elakkiyan <elakkiyan2006@gmail.com>
2023-01-03 00:08:20 -0500

    Added animations to player, and title screen.

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ from sys import exit
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load(
-            "Assets/Graphics/Player/Basic Sprite.PNG").convert_alpha(), 0, 0.75)
+        self.player_list = ["Assets/Graphics/Player/Basic Sprite.PNG",
+                            "Assets/Graphics/Player/Basic Sprite 2.PNG",
+                            "Assets/Graphics/Player/Basic Sprite 3.PNG",
+                            "Assets/Graphics/Player/Basic Sprite 4.PNG"]
+        self.player_list_index = 0
+
+        self.image = pygame.transform.rotozoom(
+            pygame.image.load(self.player_list[self.player_list_index]).convert_alpha(), 0, 0.75)
         self.rect = self.image.get_rect(center=(200, 200))
         global playerXpos
         global playerYpos
@@ -45,13 +51,22 @@ class Player(pygame.sprite.Sprite):
         self.player_movement()
         self.boundaries()
         self.update_global_player_pos()
+        self.animation()
+
+    def animation(self):
+        if self.player_list_index <= 3:
+            self.player_list_index += .05
+        else:
+            self.player_list_index = 0
+        self.image = pygame.transform.rotozoom(
+            pygame.image.load(self.player_list[int(self.player_list_index)]).convert_alpha(), 0, 0.75)
 
 
 class Eyes(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("Assets/Graphics/Player/Eyes.PNG").convert_alpha()
-        self.rect = self.image.get_rect(center=(playerXpos + 2, playerYpos + 50))
+        self.rect = self.image.get_rect(center=(0, 0))
 
     def eye_movement(self):
         mouseX, mouseY = pygame.mouse.get_pos()
@@ -65,20 +80,28 @@ class Eyes(pygame.sprite.Sprite):
             distanceY = -80
         if distanceY >= 15:
             distanceY = 15
-        # if distanceY
         self.rect.x -= distanceX / -12
         self.rect.y -= distanceY / -12
-        pass
 
     def update(self):
-        self.rect.center = (playerXpos + 2, playerYpos - 50)
+        self.target()
         self.eye_movement()
+        # self.target() sets the position, and then self.eye_movement() moves it within the boundaries
+
+    def target(self):
+        if game_active:
+            self.rect.center = (playerXpos + 2, playerYpos - 50)
+
+        if not game_active:
+            self.rect.center = (start_menu_player_rect.centerx + 2, start_menu_player_rect.centery - 75)
+            pass
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("Assets/Graphics/Penguin/Penguin Front 2.PNG").convert_alpha(), 0, 4)
+        self.image = pygame.transform.rotozoom(
+            pygame.image.load("Assets/Graphics/Penguin/Penguin Front 2.PNG").convert_alpha(), 0, 4)
         self.rect = self.image.get_rect(center=(600, 600))
 
     def ai(self):
@@ -88,7 +111,6 @@ class Enemy(pygame.sprite.Sprite):
         angle = atan2(distanceY, distanceX)
         enemyXdir = cos(angle)
         enemyYdir = sin(angle)
-        print(enemyXdir, enemyYdir)
         self.rect.centerx += enemyXdir * speed
         self.rect.centery += enemyYdir * speed
 
@@ -102,17 +124,41 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.bottom >= 800:
             self.rect.bottom = 800
 
+    def spawning(self):
+        pass
+
     def update(self):
         self.ai()
         self.boundaries()
 
+
+# Init and Setup
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Penguin Apocalypse")
 clock = pygame.time.Clock()
 screen.fill("#f0f49c")
+go_up = False
+go_down = True
+
+# Global Variables
 playerXpos = 0
 playerYpos = 0
+game_active = False
+font = pygame.font.Font("Assets/Font/Preahvihear-Regular.ttf", 75)
+font_smaller = pygame.font.Font("Assets/Font/Preahvihear-Regular.ttf", 50)
+
+# Game Screen Init
+start_menu_font = font.render("Penguin Apocalypse", True, "Black")
+start_menu_font_rect = start_menu_font.get_rect(center=(500, 200))
+start_menu_font_space = font_smaller.render("Press Space To Play", True, "Black")
+start_menu_font_space_rect = start_menu_font_space.get_rect(center=(500, 600))
+
+start_menu_player_list = ["Assets/Graphics/Player/Basic Sprite.PNG", "Assets/Graphics/Player/Basic Sprite 2.PNG",
+                          "Assets/Graphics/Player/Basic Sprite 3.PNG", "Assets/Graphics/Player/Basic Sprite 4.PNG"]
+start_menu_player_list_index = 0
+start_menu_player = pygame.image.load(start_menu_player_list[start_menu_player_list_index])
+start_menu_player_rect = start_menu_player.get_rect(center=(500, 410))
 
 # Class Group
 player = pygame.sprite.GroupSingle()
@@ -122,25 +168,60 @@ eyes.add(Eyes())
 enemy = pygame.sprite.GroupSingle()
 enemy.add(Enemy())
 
+animation_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(animation_timer, 1000)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+        if not game_active:
+            if event.type == animation_timer:
+                if start_menu_player_list_index <= 2:
+                    start_menu_player_list_index += 1
+                else:
+                    start_menu_player_list_index = 0
+                start_menu_player = pygame.image.load(start_menu_player_list[start_menu_player_list_index])
+                screen.blit(start_menu_player, start_menu_player_rect)
 
-    screen.fill("#f8f4ec")
+    if game_active:
+        screen.fill("#f8f4ec")
+        player.update()
+        player.draw(screen)
 
-    player.update()
-    player.draw(screen)
+        eyes.update()
+        eyes.draw(screen)
 
-    eyes.update()
-    eyes.draw(screen)
+        enemy.update()
+        enemy.draw(screen)
 
-    enemy.update()
-    enemy.draw(screen)
+    else:
+        screen.fill("#f8f4ec")
+        screen.blit(start_menu_font, start_menu_font_rect)
+        screen.blit(start_menu_font_space, start_menu_font_space_rect)
+        screen.blit(start_menu_player, start_menu_player_rect)
+        eyes.update()
+        eyes.draw(screen)
+        if go_down:
+            start_menu_font_rect.centery += 1
+        if start_menu_font_rect.centery == 220:
+            go_down = False
+            go_up = True
+        if go_up:
+            start_menu_font_rect.centery -= 1
+        if start_menu_font_rect.centery == 200:
+            go_down = True
+            go_up = False
 
     pygame.display.update()
-    clock.tick(60)
+    if not game_active:
+        clock.tick(25)
+    else:
+        clock.tick(60)
 
 if __name__ == '__main__':
     print('PyCharm')
